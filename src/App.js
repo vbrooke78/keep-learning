37fbb667630000953498c509d202435a7ab187c4
vbrooke78 <vscotter@hotmail.com>
2022-06-15 15:18:57 +0100

    Validation added to form - still need to display

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header';
@@ -6,13 +7,21 @@ import { LoginPage } from './components/LogInPage';
 import { Nav } from './components/Nav';
 
 function App() {
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
   return (
     <div className="App">
       <Header />
       <Nav />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/login"
+          element={<LoginPage userData={userData} setUserData={setUserData} />}
+        />
       </Routes>
     </div>
   );
