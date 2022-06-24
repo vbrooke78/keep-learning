d887f9fe44c4c2f49a3de2e6fc161f58328b99dd
vbrooke78 <vscotter@hotmail.com>
2022-06-24 15:06:06 +0100

    Validation completed on LoginTask using Formik

--- src/components/loginTask/LogInPage.jsx
+++ src/components/loginTask/LogInPage.jsx
@@ -1,56 +1,94 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoginForm } from './LoginForm';
-import { validateEmail } from './utils/ValidateForm';
-import { validatePassword } from './utils/ValidateForm';
+import { Formik } from 'formik';
 
 export const LoginPage = ({ userData, setUserData }) => {
   const [showSignUp, setShowSignUp] = useState(false);
+
   const navigate = useNavigate();
-
-  const handleValidateEmail = (e) => {
-    setUserData({ email: e.target.value });
-    validateEmail(e.target.value);
-  };
-
-  const handleValidatePassword = (e) => {
-    setUserData({ password: e.target.value });
-    validatePassword(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    navigate('/loginSuccess');
-  };
 
   return (
     <>
       <h1>Login Page</h1>
-      <form className="loginForm" onSubmit={handleSubmit}>
-        <label htmlFor="email" className="loginInput">
-          Email
-        </label>
-        <input
-          id="email"
-          type="email"
-          name="email"
-          className="loginInput"
-          value={userData.email || ''}
-          required
-          onChange={handleValidateEmail}
-        />
-        <label htmlFor="password" className="loginInput">
-          Password
-        </label>
-        <input
-          id="password"
-          type="password"
-          className="loginInput"
-          value={userData.password || ''}
-          required
-          onChange={handleValidatePassword}
-        />
-        <button className="loginInput">Submit</button>
-      </form>
+      <p>(I used Formik to validate the form fields)</p>
+      <Formik
+        initialValues={userData}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          if (!values.password) {
+            errors.password = 'Required';
+          } else if (
+            !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{3,}$/.test(
+              values.password
+            )
+          ) {
+            errors.password =
+              'Password must contain at least 1 uppercase letter, 1 lowercase letter and 1 number';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setUserData(values);
+          setTimeout(() => {
+            setSubmitting(false);
+            navigate('/loginSuccess');
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form className="loginForm" onSubmit={handleSubmit}>
+            <label htmlFor="email" className="loginInput">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              className="loginInput"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+            <label htmlFor="password" className="loginInput">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              className="loginInput"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+            <button
+              className="loginInput"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
       <div className="signUpButton">
         <button
           className="signUpInput"
--- src/components/loginTask/LoginForm.jsx
+++ src/components/loginTask/LoginForm.jsx
@@ -1,64 +1,100 @@
 import { useNavigate } from 'react-router-dom';
-import { validateName } from './utils/ValidateForm';
-import { validateEmail } from './utils/ValidateForm';
-import { validatePassword } from './utils/ValidateForm';
+import { Formik } from 'formik';
 
 export const LoginForm = ({ userData, setUserData }) => {
   const navigate = useNavigate();
 
-  const handleValidateName = (e) => {
-    setUserData({ name: e.target.value });
-    validateName(e.target.value);
-  };
-
-  const handleValidateEmail = (e) => {
-    setUserData({ email: e.target.value });
-    validateEmail(e.target.value);
-  };
-
-  const handleValidatePassword = (e) => {
-    setUserData({ password: e.target.value });
-    validatePassword(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    navigate('/signUpSuccess');
-  };
-
   return (
-    <form className="signUpForm" onSubmit={handleSubmit}>
-      <label htmlFor="name" className="signUpInput">
-        Your name
-      </label>
-      <input
-        id="name"
-        className="signUpInput"
-        value={userData.name || ''}
-        required
-        onChange={handleValidateName}
-      />
-      <label htmlFor="email" className="signUpInput">
-        Your email
-      </label>
-      <input
-        id="email"
-        type="email"
-        className="signUpInput"
-        value={userData.email || ''}
-        required
-        onChange={handleValidateEmail}
-      />
-      <label htmlFor="password" type="password" className="signUpInput">
-        Password
-      </label>
-      <input
-        id="password"
-        className="signUpInput"
-        value={userData.password || ''}
-        required
-        onChange={handleValidatePassword}
-      />
-      <button className="signUpInput">Submit</button>
-    </form>
+    <Formik
+      initialValues={userData}
+      validate={(values) => {
+        const errors = {};
+        if (!values.name) {
+          errors.name = 'Required';
+        } else if (!/^(?=.*[a-zA-Z\s])[a-zA-Z\s]{3,}$/i.test(values.name)) {
+          errors.name = 'Please enter a valid name';
+        }
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        if (!values.password) {
+          errors.password = 'Required';
+        } else if (
+          !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{3,}$/.test(
+            values.password
+          )
+        ) {
+          errors.password =
+            'Password must contain at least 1 uppercase letter, 1 lowercase letter and 1 number';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setUserData(values);
+        console.log(userData);
+        setTimeout(() => {
+          setSubmitting(false);
+          navigate('/signUpSuccess');
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <form className="signUpForm" onSubmit={handleSubmit}>
+          <label htmlFor="name" className="signUpInput">
+            Your name
+          </label>
+          <input
+            id="name"
+            name="name"
+            className="signUpInput"
+            value={values.name || ''}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          {errors.name && touched.name && errors.name}
+          <label htmlFor="email" className="signUpInput">
+            Your email
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className="signUpInput"
+            value={values.email || ''}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          {errors.email && touched.email && errors.email}
+          <label htmlFor="password" type="password" className="signUpInput">
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className="signUpInput"
+            value={values.password || ''}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          {errors.password && touched.password && errors.password}
+          <button className="signUpInput" type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
   );
 };
